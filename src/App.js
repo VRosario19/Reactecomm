f12a40f10bdf198b2def26241f49cabdc43000d5
Vanessa Rosario <vanessarosario@AdminisatorsMBP.attlocal.net>
2021-01-26 12:11:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./Component/HomePage/HomePage.js";
+import ProductsPage from "./Component/ProductsPage/ProductsPage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Products">Products</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+        A <Switch> looks through all its children <Route>
+        elements and renders the first one whose path
+        matches the current URL. Use a <Switch> any time
+        you have multiple routes, but you want only one
+        of them to render at a time
+      */}
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/Products">
+            <ProductsPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Component/HomePage/HomePage.js
@@ -0,0 +1,45 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Slider from "react-slick";
+import "./HomePage.css";
+import images from "./images/Household.jpg";
+
+const HomePage = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <img
+          className="Household"
+          src={"./Components/images/Household.jpg"}
+          alt=""
+        />
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
+  );
+};
+
+export default HomePage;
